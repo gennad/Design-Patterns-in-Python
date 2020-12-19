231a06a9e770d658e8803d1a4e8552ad8a404e00
Sai Krishna <saimanthena13579@gmail.com>
2020-12-19 18:24:52 +0530

    fixed error in the print statement
    print statement in line 32 did not have a closed bracket

--- factory.py
+++ factory.py
@@ -29,5 +29,5 @@ class PizzaFactory(object):
 
 if __name__ == '__main__':
     for pizza_type in ('HamMushroom', 'Deluxe', 'Hawaiian'):
-          print('Price of {0} is {1}'.format(pizza_type, PizzaFactory.create_pizza(pizza_type).get_price())
+          print('Price of {0} is {1}'.format(pizza_type, PizzaFactory.create_pizza(pizza_type).get_price()))
 
